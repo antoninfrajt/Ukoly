4141b227c3c10950e1fc191348c42fe64336fd3f
antoninfrajt <147373979+antoninfrajt@users.noreply.github.com>
2024-04-15 06:42:39 +0200

    Add files via upload

---
+++ cpp/zkouska/classes/book.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self,s:str):
+        self.id = int(s.split(";")[0])
+        self.name = s.split(";")[1]
+        self.author = s.split(";")[2]
+        if s.split(";")[3] == "Available":
+            self.available = True
+        else:
+            self.available = False
+    def __str__(self)->str:
+        s = str(self.id) + ";" + self.name + ";" + self.author + ";"
+        if self.available:
+            s += "Available"
+        else:
+            s += "Unavailable"
+        return s
+    def __lt__ (self,other):
+        if self.author [0] < other.author[0]:
+            return True
+        else:
+            return False
+    def __eq__(self, other) -> bool:
+        if self.id == other.id:
+            return True
+        else:
+            return False
+    def set_id(self, id):
+        self.id = id
+
