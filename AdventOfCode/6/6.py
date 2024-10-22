d7750b00c88721f36a75a51fc63b0b1e62cad403
antoninfrajt <tfonda.ftrajt@gmail.com>
2024-10-22 12:50:54 +0200

    comit 3

--- AdventOfCode/6/6.py
+++ AdventOfCode/6/6.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from os.path import realpath, dirname, join
 def zavod(c:int,v:int)->int:
     pocet:int = 0
@@ -36,43 +35,4 @@ def parse(s:str):
             print(pocet)
             
 
-=======
-from os.path import realpath, dirname, join
-def zavod(c:int,v:int)->int:
-    pocet:int = 0
-    mv:int = 0
-    for i in range(0,c+1):
-        mv = i*(c-i)
-        if mv > v:
-             pocet += 1
-    return pocet
-def parse(s:str):
-    cislo:list =[]
-    vzd:str = ""
-    cas:str = ""
-    pocet:int = 1 
-    with open(join(dirname(realpath(__file__)),s),"r", encoding="utf_8") as file:
-            soubor:list = file.read().split('\n')
-            for x in soubor[0][6:].split(' '):
-                x.strip()
-                if x != "":
-                    cislo.append(x)
-            print(cislo)
-            cas = cas.join(cislo)
-            print(cas)
-            cislo.clear()
-            for x in soubor[1][9:].split(' '):
-                x.strip()
-                if x != "":
-                    cislo.append(x)
-            vzd  = vzd.join(cislo)
-            print(vzd)
-            print(cas)
-            cas.strip()
-            vzd.strip()
-            pocet = zavod(int(cas),int(vzd))
-            print(pocet)
-            
-
->>>>>>> 5416915f6e236b2c4f9e3ce51dee2d4b630d547c
 parse("input.txt")
--- AdventOfCode/9/9_2.py
+++ AdventOfCode/9/9_2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from os.path import realpath, dirname, join
 def konec(d:list)->bool:
     vys  = True
@@ -39,46 +38,4 @@ def parse(s:str):
                 data = predpovez(data)
                 soucet += data[0]
             print(soucet)
-=======
-from os.path import realpath, dirname, join
-def konec(d:list)->bool:
-    vys  = True
-    for x in d:
-        if x != 0:
-            vys =  False
-    return vys
-def predpovez(l:list)->list:
-    ll:list = []
-    ll.append(l)
-    doc = l
-    while konec(doc) == False:
-        l = doc
-        doc = []
-        for i in range(0,len(l)-1):
-            doc.append(l[i+1]-l[i])
-        ll.append(doc)
-    ll.reverse()
-    for i in range(0,len(ll)-1):
-        ll[i+1].reverse()
-        ll[i+1].append(ll[i+1][-1]-ll[i][0])
-        ll[i+1].reverse()
-    #for x in ll:
-        #print(x)
-    return ll[-1]
-        
-        
-def parse(s:str):
-    data:list = []
-    soucet:int = 0
-    with open(join(dirname(realpath(__file__)),s),"r", encoding="utf_8") as file:
-            soubor:list = file.read().split('\n')
-            for line in soubor:
-                data = []
-                for x in line.split(' '):
-                    data.append(int(x))
-                #print(data)
-                data = predpovez(data)
-                soucet += data[0]
-            print(soucet)
->>>>>>> 5416915f6e236b2c4f9e3ce51dee2d4b630d547c
 parse("input.txt")
